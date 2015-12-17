cde0f13f216099f1c791cdfdb623c6980b8dda7e
Лев Зибарев <lzibarev@yandex.ru>
2015-12-18 00:25:21 +0300

    Наконец-то победил инпут. Ура - работаем дальше

--- src/p1006_encoding/Solution.java
+++ src/p1006_encoding/Solution.java
@@ -1,6 +1,8 @@
 package p1006_encoding;
 
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,9 +10,8 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		PrintWriter out = new PrintWriter(System.out);
-
+		Scanner in = new Scanner(System.in, "ISO-8859-1");
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, Charset.forName("ISO-8859-1")));
 		solution(in, out);
 
 		out.flush();
@@ -29,7 +30,7 @@ public class Solution {
 			inputData.add(row);
 		}
 		Result r = hasAnyOnTop();
-		print();
+		// print();
 		while (r != null) {
 			result.add(r);
 			removeFromTop(r);
@@ -44,15 +45,13 @@ public class Solution {
 		}
 	}
 
-	// '|', '*'
-	private static List<Character> leftRight = Arrays.asList((char) 179, '|', 'a');
-	// '*'
-	private static List<Character> topButtom = Arrays.asList((char) 196, '-', 'a');
+	private static List<Character> leftRight = Arrays.asList((char) CONST.l, 'a');
+	private static List<Character> topButtom = Arrays.asList((char) CONST.t, 'a');
 
 	private static void print() {
 		for (List<Integer> row : inputData) {
 			for (Integer i : row) {
-				System.out.print(i.intValue() + "\t");
+				System.out.print((char) i.intValue() + "\t");
 			}
 			System.out.println();
 		}
@@ -61,30 +60,49 @@ public class Solution {
 
 	private static Result hasAnyOnTop() {
 		Point p = new Point();
-		p.x = 0;
+		p.y = 0;
 		for (List<Integer> row : inputData) {
-			p.y = 0;
+			p.x = 0;
 			for (Integer i : row) {
-				if (i == 218 || i == 9484) {
-					for (int length = 1; length <= row.size() - p.y && length <= inputData.size() - p.x; length++) {
-						int end = inputData.get(p.x + length).get(p.y + length);
-						if ((char) end == 217 || end == 9496) {
+				boolean hasReal = false;
+				if (i == CONST.tl || i == 'a') {
+					hasReal |= i == CONST.tl;
+					for (int length = 1; length < row.size() - p.x && length < inputData.size() - p.y; length++) {
+						int end = inputData.get(p.y + length).get(p.x + length);
+						if (end == CONST.br || end == 'a') {
+							hasReal |= end == CONST.br;
 							boolean isCorrect = true;
+							int tr = inputData.get(p.y).get(p.x + length);
+							if (tr == CONST.tr || tr == 'a') {
+								hasReal |= tr == CONST.tr;
+							} else {
+								isCorrect = false;
+							}
+							int bl = inputData.get(p.y + length).get(p.x);
+							if (bl == CONST.bl || bl == 'a') {
+								hasReal |= bl == CONST.bl;
+							} else {
+								isCorrect = false;
+							}
 							for (int tempLength = 1; tempLength < length; tempLength++) {
-								int left = inputData.get(p.x + tempLength).get(p.y);
-								int right = inputData.get(p.x + tempLength).get(p.y + length);
-								int top = inputData.get(p.x).get(p.y + tempLength);
-								int buttom = inputData.get(p.x + length).get(p.y + tempLength);
+								int left = inputData.get(p.y + tempLength).get(p.x);
+								int right = inputData.get(p.y + tempLength).get(p.x + length);
+								int top = inputData.get(p.y).get(p.x + tempLength);
+								int buttom = inputData.get(p.y + length).get(p.x + tempLength);
 								if (!leftRight.contains((char) left) && !leftRight.contains((char) right)) {
+									hasReal |= left != 'a';
+									hasReal |= right != 'a';
 									isCorrect = false;
 									break;
 								}
 								if (!topButtom.contains((char) top) && !topButtom.contains((char) buttom)) {
+									hasReal |= top != 'a';
+									hasReal |= buttom != 'a';
 									isCorrect = false;
 									break;
 								}
 							}
-							if (isCorrect) {
+							if (isCorrect && hasReal) {
 								Result r = new Result();
 								r.length = length + 1;
 								r.p = p;
@@ -93,26 +111,32 @@ public class Solution {
 						}
 					}
 				}
-				p.y++;
+				p.x++;
 			}
-			p.x++;
+			p.y++;
 		}
 		return null;
 	}
 
+	private static class CONST {
+		private static int tl = 218, t = 196, tr = 191;
+		private static int l = 179, r = 179;
+		private static int bl = 192, b = 196, br = 217;
+	}
+
 	private static void removeFromTop(Result r) {
-		for (int y = r.p.y; y < r.p.y + r.length; y++) {
-			inputData.get(r.p.x).set(y, (int) '*');
-			inputData.get(r.p.x + r.length - 1).set(y, (int) '*');
+		for (int y = r.p.x; y < r.p.x + r.length; y++) {
+			inputData.get(r.p.y).set(y, (int) 'a');
+			inputData.get(r.p.y + r.length - 1).set(y, (int) 'a');
 		}
-		for (int x = r.p.x; x < r.p.x + r.length; x++) {
-			inputData.get(x).set(r.p.y, (int) '*');
-			inputData.get(x).set(r.p.y + r.length - 1, (int) '*');
+		for (int x = r.p.y; x < r.p.y + r.length; x++) {
+			inputData.get(x).set(r.p.x, (int) 'a');
+			inputData.get(x).set(r.p.x + r.length - 1, (int) 'a');
 		}
 	}
 
 	private static class Point {
-		private int x, y;
+		private int y, x;
 	}
 
 	private static class Result {
--- src/p1006_encoding/SolutionTest.java
+++ src/p1006_encoding/SolutionTest.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.Scanner;
 
@@ -30,41 +29,32 @@ public class SolutionTest {
 
 	@Test
 	public void test1() throws Exception {
-		for (Charset charset : Charset.availableCharsets().values()) {
-			System.out.println(charset);
-			// Files.readAllLines(new File("src/p1006/input1.txt").toPath(),
-			// charset).stream()
-			// .forEach(s -> input += s + "\r\n");
-			Charset cs = Charset.forName("ASCII");
-			;
-			System.out.println(Files.readAllLines(new File("src/p1006/input1.txt").toPath(), cs));
-			return;
-			// System.out.println(charset.decode(ByteBuffer.wrap(b)));
-		}
-		if (true) {
-			return;
-		}
-		System.out.println();
-		Files.readAllLines(new File("src/p1006/input1.txt").toPath(), Charset.forName("windows-1251")).stream()
-				.forEach(s -> input += s + "\r\n");
-		System.out.println(input);
-		Files.readAllLines(new File("src/p1006/output1.txt").toPath()).stream().forEach(s -> expected += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/input1.txt").toPath()).stream().forEach(s -> input += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/output1.txt").toPath()).stream().forEach(s -> expected += s + System.lineSeparator());
 		output = run(input);
 		Assert.assertEquals(expected, output);
 	}
 
 	@Test
 	public void test2() throws Exception {
-		Files.readAllLines(new File("src/p1006/input2.txt").toPath()).stream().forEach(s -> input += s + "\r\n");
-		Files.readAllLines(new File("src/p1006/output2.txt").toPath()).stream().forEach(s -> expected += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/input2.txt").toPath()).stream().forEach(s -> input += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/output2.txt").toPath()).stream().forEach(s -> expected += s + System.lineSeparator());
 		output = run(input);
 		Assert.assertEquals(expected, output);
 	}
 
 	@Test
 	public void test3() throws Exception {
-		Files.readAllLines(new File("src/p1006/input3.txt").toPath()).stream().forEach(s -> input += s + "\r\n");
-		Files.readAllLines(new File("src/p1006/output3.txt").toPath()).stream().forEach(s -> expected += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/input3.txt").toPath()).stream().forEach(s -> input += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/output3.txt").toPath()).stream().forEach(s -> expected += s + System.lineSeparator());
+		output = run(input);
+		Assert.assertEquals(expected, output);
+	}
+
+	@Test
+	public void test4() throws Exception {
+		Files.readAllLines(new File("src/p1006_encoding/input4.txt").toPath()).stream().forEach(s -> input += s + "\r\n");
+		Files.readAllLines(new File("src/p1006_encoding/output4.txt").toPath()).stream().forEach(s -> expected += s + System.lineSeparator());
 		output = run(input);
 		Assert.assertEquals(expected, output);
 	}
