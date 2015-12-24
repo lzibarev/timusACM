3104c436e06e29dcb8519f84d18de4b53e20ce75
lzibarev <lzibarev@lzibarev2.luxoft.com>
2015-12-24 06:55:24 -0500

    1030 - Accept

--- src/p1030/Solution.java
+++ src/p1030/Solution.java
@@ -1,6 +1,8 @@
 package p1030;
 
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -16,19 +18,49 @@ public class Solution {
 	}
 
 	protected static void solution(Scanner in, PrintWriter out) {
-		int k = in.nextInt();
-		int maxDevitor = k / 2 + 3;
-		long ans = k - 1;
-		for (int i = 3; i < maxDevitor; i++) {
-			if (k % i == 0) {
-				ans = i - 1;
-				break;
-			}
+		while (in.hasNext()) {
+			// Message #<n>.
+			in.nextLine();
+			// Received at <HH>:<MM>:<SS>.
+			in.nextLine();
+			// Current ship's coordinates are
+			in.nextLine();
+			// <X1>^<X2>'<X3>" <NL/SL>
+			String p1X = in.nextLine();
+			// and <Y1>^<Y2>'<Y3>" <EL/WL>.
+			String p1Y = in.nextLine();
+			// An iceberg was noticed at
+			in.nextLine();
+			// <A1>^<A2>'<A3>" <NL/SL>
+			String p2X = in.nextLine();
+			// and <B1>^<B2>'<B3>" <EL/WL>.
+			String p2Y = in.nextLine();
+			// ===
+			in.nextLine();
+
+			point p1 = new point(p1X, p1Y);
+			point p2 = new point(p2X, p2Y);
+
+			d = calculateD(p1, p2);
+			print(out, d);
 		}
-		out.println(ans);
 	}
 
-	protected static String calculate(point p1, point p2) {
+	protected static String print(PrintWriter out, double d) {
+		DecimalFormat df = new DecimalFormat("0.00", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+		d = Math.round(d * 100) / 100d;
+		// System.out.println(df.format(d));
+		String ans = "The distance to the iceberg: " + df.format(d) + " miles.";
+		// System.out.println(d);
+		out.println(ans);
+		if (d < 100) {
+			out.println("DANGER!");
+			ans += "DANGER!";
+		}
+		return ans;
+	}
+
+	protected static double calculateD(point p1, point p2) {
 		double x1 = p1.getRadianX();
 		double y1 = p1.getRadianY();
 
@@ -53,8 +85,11 @@ public class Solution {
 		double atan2bottom = Math.sin(radLat1) * Math.sin(radLat2)
 				+ Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radLonDif);
 		double deltaAngle = Math.atan2(atan2top, atan2bottom);
-		double d2 = r * deltaAngle;
+		return r * deltaAngle;
+	}
 
+	protected static String calculate(point p1, point p2) {
+		double d2 = calculateD(p1, p2);
 		return String.format(Locale.ENGLISH, "%.2f", d2);
 	}
 
@@ -62,14 +97,41 @@ public class Solution {
 	private static double r = d / 2;
 
 	protected static class point {
+		int sx = 1, sy = 1;
 		double x1, x2, x3, y1, y2, y3;
 
+		point() {
+		}
+
+		point(String xstr, String ystr) {
+			// <X1>^<X2>'<X3>" <NL/SL>
+			// and <Y1>^<Y2>'<Y3>" <EL/WL>.
+			String[] x1Str = xstr.trim().split("\\^");
+			x1 = Integer.parseInt(x1Str[0].trim());
+			String[] x2Str = x1Str[1].split("\\'");
+			x2 = Integer.parseInt(x2Str[0].trim());
+			String[] x3Str = x2Str[1].split("\\\"");
+			x3 = Integer.parseInt(x3Str[0].trim());
+			sx = x3Str[1].trim().startsWith("NL") ? 1 : -1;
+
+			String[] y1Str = ystr.trim().split("\\^");
+			y1 = Integer.parseInt(y1Str[0].substring("and ".length()).trim());
+			String[] y2Str = y1Str[1].split("\\'");
+			y2 = Integer.parseInt(y2Str[0].trim());
+			String[] y3Str = y2Str[1].split("\\\"");
+			y3 = Integer.parseInt(y3Str[0].trim());
+			sy = y3Str[1].trim().startsWith("EL") ? 1 : -1;
+
+			// System.out.println(toString());
+
+		}
+
 		double degreeX() {
-			return x1 + x2 / 60 + x3 / 3600;
+			return sx * (x1 + x2 / 60 + x3 / 3600);
 		}
 
 		double degreeY() {
-			return y1 + y2 / 60 + y3 / 3600;
+			return sy * (y1 + y2 / 60 + y3 / 3600);
 		}
 
 		double getRadianX() {
@@ -78,6 +140,11 @@ public class Solution {
 
 		double getRadianY() {
 			return Math.toRadians(degreeY());
+		}
+
+		@Override
+		public String toString() {
+			return degreeX() + " " + degreeY();
 		}
 	}
 }
--- src/p1030/SolutionTest.java
+++ src/p1030/SolutionTest.java
@@ -28,6 +28,98 @@ public class SolutionTest {
 	}
 
 	@Test
+	public void pritnTest() {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		PrintWriter out = new PrintWriter(os);
+		output = Solution.print(out, 100);
+		Assert.assertEquals("The distance to the iceberg: 100.00 miles.", output);
+
+		output = Solution.print(out, 100.001);
+		Assert.assertEquals("The distance to the iceberg: 100.00 miles.", output);
+
+		output = Solution.print(out, 100.005);
+		Assert.assertEquals("The distance to the iceberg: 100.01 miles.", output);
+
+		output = Solution.print(out, 100.01);
+		Assert.assertEquals("The distance to the iceberg: 100.01 miles.", output);
+	}
+
+	private String str(String x, String y, String a, String b) {
+		return "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n " + x + " \r\nand " + y
+				+ ".\r\nAn iceberg was noticed at\r\n" + a + " \r\nand " + b + ".\r\n=== \r\n";
+	}
+
+	@Test
+	public void test0() {
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'00\" NL \r\nand 50^14'00\" WL.\r\nAn iceberg was noticed at\r\n41^14'11\" NL \r\nand 51^09'00\" WL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 52.04 miles.\r\nDANGER!", output);
+	}
+
+	@Test
+	public void testZero() {
+		input = str("00^00'00\" NL", "00^00'00\" WL", "00^00'00\" NL", "00^00'00\" WL");
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 0.00 miles.\r\nDANGER!", output);
+
+		input = str("00^00'01\" NL", "00^00'00\" WL", "00^00'00\" NL", "00^00'00\" WL");
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 0.02 miles.\r\nDANGER!", output);
+
+		input = str("00^00'00\" NL", "00^00'00\" WL", "00^00'01\" NL", "00^00'00\" WL");
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 0.02 miles.\r\nDANGER!", output);
+
+		input = str("00^00'00\" NL", "00^00'00\" WL", "00^00'02\" NL", "00^00'00\" WL");
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 0.03 miles.\r\nDANGER!", output);
+
+		input = str("00^00'01\" SL", "00^00'00\" WL", "00^00'01\" NL", "00^00'00\" WL");
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 0.03 miles.\r\nDANGER!", output);
+
+		input = str("00^00'00\" SL", "00^00'01\" WL", "00^00'00\" NL", "00^00'01\" EL");
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 0.03 miles.\r\nDANGER!", output);
+	}
+
+	@Test
+	public void test0_round() {
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'10\" NL \r\nand 50^14'00\" WL.\r\nAn iceberg was noticed at\r\n41^14'11\" NL \r\nand 51^09'00\" WL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 52.15 miles.\r\nDANGER!", output);
+
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'11\" NL \r\nand 50^14'00\" WL.\r\nAn iceberg was noticed at\r\n41^14'11\" NL \r\nand 51^09'00\" WL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 52.16 miles.\r\nDANGER!", output);
+
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'12\" NL \r\nand 50^14'00\" WL.\r\nAn iceberg was noticed at\r\n41^14'11\" NL \r\nand 51^09'00\" WL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 52.17 miles.\r\nDANGER!", output);
+	}
+
+	@Test
+	public void test0_1() {
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'00\" NL \r\nand 50^14'00\" EL.\r\nAn iceberg was noticed at\r\n41^14'11\" NL \r\nand 51^09'00\" EL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 52.04 miles.\r\nDANGER!", output);
+	}
+
+	@Test
+	public void test0_2() {
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'00\" SL \r\nand 50^14'00\" EL.\r\nAn iceberg was noticed at\r\n41^14'11\" SL \r\nand 51^09'00\" EL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 52.04 miles.\r\nDANGER!", output);
+	}
+
+	@Test
+	public void test0_3() {
+		input = "Message #513.\r\nReceived at 22:30:11. \r\nCurrent ship’s coordinates are\r\n 41^46'00\" NL \r\nand 50^14'00\" EL.\r\nAn iceberg was noticed at\r\n41^14'11\" SL \r\nand 51^09'00\" EL.\r\n=== \r\n";
+		output = run(input);
+		Assert.assertEquals("The distance to the iceberg: 4980.07 miles.", output);
+	}
+
+	@Test
 	public void test() {
 		point p1 = new point();
 		point p2 = new point();
